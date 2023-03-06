eae18243c344a756c11d7403424bb9d1edf438b2
dynaput247 <broweqos302679@hotmail.com>
2023-03-06 13:06:30 +0100

    Change data path

--- src/ScoreTable.tsx
+++ src/ScoreTable.tsx
@@ -17,7 +17,7 @@ export default function ScoreTable() {
     }
 
     useEffect(() => {
-        fetch('/data/user-data.json')
+        fetch('./data/user-data.json')
             .then((res) => res.json())
             .then((data) => setScores(data))
             .catch((err) => console.error(err))
